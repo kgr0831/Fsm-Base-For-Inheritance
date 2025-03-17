1028364788e24892bbb98f9b182970023c1f067a
kgr0831 <kimgalam0831@gmail.com>
2025-03-17 15:53:40 +0900

    Add files via upload

---
+++ FsmBase.cs
@@ -0,0 +1,26 @@
+public class FsmBase
+{
+    public delegate void StateAction();
+
+    public class State
+    {
+        public StateAction OnEnter;
+        public StateAction OnExit;
+        public StateAction OnUpdate;
+
+        public void Enter() => OnEnter?.Invoke();
+        public void Exit() => OnExit?.Invoke();
+        public void Update() => OnUpdate?.Invoke();
+    }
+
+    protected State _currentState;
+
+    public void SetState(State newState)
+    {
+        _currentState?.Exit();
+        _currentState = newState;
+        _currentState.Enter();
+    }
+
+    public void Update() => _currentState?.Update();
+}
